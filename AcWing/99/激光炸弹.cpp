be001d8557d32d9861be4a979430f3b7d1c5da26
lyccrius <lyccrius@outlook.com>
2022-08-26 17:16:16 +0800

    updatE

---
+++ AcWing/99/激光炸弹.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, R;
+int X, Y, W;
+int S[5010][5010];
+
+int main() {
+    cin >> N >> R;
+
+    R = min(R, 5001);
+
+    int n = R, m = R;
+
+    while (N --) {
+        cin >> X >> Y >> W;
+        S[++ X][++ Y] += W;
+        n = max(n, X);
+        m = max(m, Y);
+    }
+
+    for (int i = 1; i <= 5001; i ++) {
+        for (int j = 1; j <= 5001; j ++) {
+            S[i][j] += S[i - 1][j] + S[i][j - 1] - S[i - 1][j - 1];
+        }
+    }
+
+    int ans = 0;
+
+    for (int i = R; i <= 5001; i ++) {
+        for (int j = R; j <= 5001; j ++) {
+            ans = max(ans, S[i][j] - S[i - R][j] - S[i][j - R] + S[i - R][j - R]);
+        }
+    }
+
+    cout << ans << endl;
+
+    return 0;
+}
