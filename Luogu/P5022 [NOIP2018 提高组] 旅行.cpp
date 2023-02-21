f564a9319693e40364a8ab4521528a8328f0cf72
Lyccrius <lyccrius@outlook.com>
2023-02-21 18:42:22 +0800

    update

---
+++ Luogu/P5022 [NOIP2018 提高组] 旅行.cpp
@@ -0,0 +1,153 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+
+typedef std::vector<std::pair<int, int> > vpc;
+typedef std::vector<int> vic;
+typedef std::stack<int> sic;
+
+const int maxN = 5000;
+const int maxM = 5000;
+
+int n, m;
+int u, v;
+vic res;
+vic ans;
+
+namespace graph {
+    struct Vertex {
+        int head;
+        vic edge;
+        int dfn;
+        int low;
+        int ins;
+    } vertex[maxN + 10];
+
+    struct Edge {
+        int head;
+        int next;
+    } edge[maxM * 2 + 10];
+
+    int ecnt;
+
+    void addEdge(int tail, int head) {
+        ecnt++;
+        edge[ecnt].head = head;
+        edge[ecnt].next = vertex[tail].head;
+        vertex[tail].head = ecnt;
+        return;
+    }
+
+    namespace ring {
+        int dfn;
+        sic s;
+        vpc r;
+
+        void addSCC(int u) {
+            int v;
+            int w = u;
+            do {
+                if (s.empty()) return;
+                v = s.top();
+                s.pop();
+                vertex[v].ins = false;
+                if (v != w) r.push_back(std::make_pair(v, w));
+                w = v;
+            } while (v != u);
+            return;
+        }
+
+        void Tarjan(int u, int from) {
+            dfn++;
+            vertex[u].dfn = dfn;
+            vertex[u].low = dfn;
+            vertex[u].ins = true;
+            s.push(u);
+            for (auto v : vertex[u].edge) {
+                if (v == from) continue;
+                if (!vertex[v].dfn) {
+                    Tarjan(v, u);
+                    vertex[u].low = std::min(vertex[u].low, vertex[v].low);
+                } else if (vertex[v].ins) {
+                    vertex[u].low = std::min(vertex[u].low, vertex[v].dfn);
+                }
+            }
+            if (vertex[u].dfn == vertex[u].low) addSCC(u);
+            return;
+        }
+
+        bool check() {
+            if (ans.empty()) return true;
+            for (int i = 0; i < n; i++) {
+                if (ans[i] < res[i]) return false;
+                if (ans[i] > res[i]) return true;
+            }
+            return false;
+        }
+
+        void update() {
+            ans.clear();
+            for (int i = 0; i < n; i++) {
+                ans.push_back(res[i]);
+            }
+            return;
+        }
+
+        void DFS(int u, int from, int fir, int sec) {
+            res.push_back(u);
+            for (auto v : vertex[u].edge) {
+                if (v == from) continue;
+                if (u == fir && v == sec) continue;
+                if (u == sec && v == fir) continue;
+                DFS(v, u, fir, sec);
+            }
+            return;
+        }
+
+        void mian() {
+            Tarjan(1, 0);
+            for (auto e : r) {
+                res.clear();
+                DFS(1, 0, e.first, e.second);
+                if (check()) update();
+            }
+            return;
+        }
+    }
+
+    namespace tree {
+        void DFS(int u, int from) {
+            ans.push_back(u);
+            for (auto v : vertex[u].edge) {
+                if (v == from) continue;
+                DFS(v, u);
+            }
+            return;
+        }
+        
+        void mian() {
+            DFS(1, 0);
+            return;
+        }
+    }
+
+    void mian() {
+        for (int u = 1; u <= n; u++) std::sort(vertex[u].edge.begin(), vertex[u].edge.end());
+        if (n == m) ring::mian();
+        if (n != m) tree::mian();
+        for (auto u : ans) std::cout << u << ' ';
+        return;
+    }
+}
+
+int main() {
+    std::cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        std::cin >> u >> v;
+        graph::vertex[u].edge.push_back(v);
+        graph::vertex[v].edge.push_back(u);
+    }
+    graph::mian();
+    return 0;
+}
---
+++ LycoReco/NOIPS2014D2T2  寻找道路 SPFA.cpp
@@ -0,0 +1,96 @@
+#include <iostream>
+#include <queue>
+
+const int maxN = 1e4;
+const int maxM = 2e5;
+const int inf = 1e9;
+
+int n, m;
+int x, y;
+int s, t;
+
+struct Graph {
+    struct Vertex {
+        int vertex;
+        int head;
+        int vis;
+        int dis;
+        int ok;
+    } vertex[maxN + 10];
+
+    struct Edge {
+        int head;
+        int next;
+    } edge[maxM + 10];
+
+    int ecnt;
+
+    void addEdge(int tail, int head) {
+        ecnt++;
+        edge[ecnt].head = head;
+        edge[ecnt].next = vertex[tail].head;
+        vertex[tail].head = ecnt;
+        return;
+    }
+
+    void SPFA(int s) {
+        std::queue<int> q;
+        for (int u = 1; u <= n; u++) vertex[u].dis = inf;
+        vertex[s].dis = 0;
+        q.push(s);
+        while (!q.empty()) {
+            int u = q.front();
+            q.pop();
+            if (vertex[u].vis) continue;
+            vertex[u].vis = true;
+            for (int e = vertex[u].head; e; e = edge[e].next) {
+                int v = edge[e].head;
+                if (!vertex[v].ok) continue;
+                if (vertex[v].dis > vertex[u].dis + 1) {
+                    vertex[v].dis = vertex[u].dis + 1;
+                    q.push(v);
+                }
+            }
+        }
+        return;
+    }
+} G, R;
+
+void BFS(int u) {
+    std::queue<int> q;
+    q.push(t);
+    while (!q.empty()) {
+        int u = q.front();
+        q.pop();
+        if (R.vertex[u].vis) continue;
+        R.vertex[u].vis = true;
+        for (int e = R.vertex[u].head; e; e = R.edge[e].next) {
+            int v = R.edge[e].head;
+            q.push(v);
+        }
+    }
+    for (int u = 1; u <= n; u++) if (R.vertex[u].vis) G.vertex[u].ok = true;
+    for (int u = 1; u <= n; u++) {
+        if (R.vertex[u].vis) continue;
+        for (int e = R.vertex[u].head; e; e = R.edge[e].next) {
+            int v = R.edge[e].head;
+            G.vertex[v].ok = false;
+        }
+    }
+    return;
+}
+
+int main() {
+    std::cin >> n >> m;
+    for (int i = 1; i <= m; i++) {
+        std::cin >> x >> y;
+        G.addEdge(x, y);
+        R.addEdge(y, x);
+    }
+    std::cin >> s >> t;
+    BFS(t);
+    G.SPFA(s);
+    if (G.vertex[t].dis == inf) G.vertex[t].dis = -1;
+    std::cout << G.vertex[t].dis;
+    return 0;
+}
