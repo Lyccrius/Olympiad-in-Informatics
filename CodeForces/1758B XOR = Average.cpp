b9a146b334b5f7dc6e1a18f5b1dbbd79ef68fad8
Lyccrius <lyccrius@outlook.com>
2023-02-26 20:48:28 +0800

    update

--- CodeForces/1758B XOR = Average.cpp
+++ CodeForces/1758B XOR = Average.cpp
@@ -1,1 +1,19 @@
+#include <cstdio>
  
+int t;
+int n;
+ 
+int main() {
+	scanf("%d", &t);
+	while (t--) {
+		scanf("%d", &n);
+		if (n & 1) {
+            for (int i = 1; i <= n; i++) printf("%d ", 1);
+        } else {
+            printf("%d ", 1);
+            for (int i = 2; i <= n; i++) printf("%d ", n + 1);
+        }
+		printf("\n");
+	}
+	return 0;
+}
