4394ee7f07ca6b394d00c7589f91ba9f0b02db5b
Lyccrius <lyccrius@outlook.com>
2023-02-26 20:48:56 +0800

    update

--- CodeForces/CF1790F Timofey and Black-White Tree.cpp
+++ CodeForces/CF1790F Timofey and Black-White Tree.cpp
@@ -50,3 +50,41 @@ namespace tree {
             DFS(v, u);
         }
         return;
+    }
+
+    void mian(int u, int dis) {
+        if (dis >= ans) return;
+        ans = std::min(ans, vertex[u].son + dis);
+        vertex[u].son = std::min(vertex[u].son, dis);
+        if (vertex[u].status && dis) return;
+        if (vertex[u].fa) mian(vertex[u].fa, dis + 1);
+        return;
+    }
+}
+
+void mian() {
+    std::cin >> n;
+    tree::init();
+    for (int i = 0; i < n; i++) std::cin >> c[i];
+    for (int i = 1; i < n; i++) {
+        std::cin >> u >> v;
+        tree::addEdge(u, v);
+        tree::addEdge(v, u);
+    }
+    tree::DFS(1, 0);
+    tree::vertex[c[0]].status = true;
+    tree::mian(c[0], 0);
+    for (int i = 1; i < n; i++) {
+        tree::vertex[c[i]].status = true;
+        tree::mian(c[i], 0);
+        std::cout << ans << ' ';
+    }
+    std::cout << '\n';
+    return;
+}
+
+int main() {
+    std::cin >> t;
+    while (t--) mian();
+    return 0;
+}
