aa12a77364bb6d429099a7253d9274ddc17df8d3
Lyccrius <lyccrius@outlook.com>
2023-03-24 13:57:08 +0800

    update

---
+++ Luogu/P1127 词链.cpp
@@ -0,0 +1,161 @@
+#include <iostream>
+#include <algorithm>
+#include <string>
+#include <deque>
+
+void promote() {
+	std::ios::sync_with_stdio(0);
+	std::cin.tie(0);
+	std::cout.tie(0);
+	return;
+}
+
+typedef std::string str;
+typedef std::deque<int> dic;
+
+const int maxN = 1000;
+
+int n;
+str m;
+int s;
+int t;
+
+namespace graph {
+	struct Vertex {
+		int head;
+		dic edge;
+		int i;
+		int o;
+	} vertex[maxN + 10];
+
+	struct Edge {
+        int ban;
+		int tail;
+		int head;
+		str text;
+	};
+
+	std::deque<Edge> edges;
+
+	void addEdge(int tail, int head, str text) {
+		edges.push_back((Edge) {false, tail, head, text});
+		vertex[tail].edge.push_back(edges.size() - 1);
+		vertex[tail].o++;
+		vertex[head].i++;
+		return;
+	}
+
+    bool cmp(const int &a, const int &b) {
+        return edges[a].text < edges[b].text;
+    }
+
+	struct Node {
+		int u;
+		int e;
+	};
+
+	std::deque<Node> ans;
+
+	void DFS(int u, int fe) {
+        //printf("DFS %c ", u + 'a' - 1); if (~fe) std::cout << edges[fe].text; printf("\n");
+		//printf("DFS %d from %d\n", u, fe);
+		for (auto e : vertex[u].edge) {
+			int v = edges[e].head;
+            if (edges[e].ban) continue;
+            edges[e].ban = true;
+			vertex[u].edge.pop_front();
+			DFS(v, e);
+		}
+		if (~fe) ans.push_back((Node) {u, fe});
+        //if (~fe) printf("ans.push "), std::cout << edges[fe].text, printf("\n");
+        //printf("DFS %c ", u + 'a' - 1); if (~fe) std::cout << edges[fe].text; printf(" end \n");
+		return;
+	}
+
+	void print() {
+        if (!ans.empty()) std::cout << edges[ans.back().e].text, ans.pop_back();
+		while (!ans.empty()) {
+            std::cout << '.';
+			std::cout << edges[ans.back().e].text;
+			ans.pop_back();
+		}
+		return;
+	}
+}
+
+int abs(int x) {
+	if (x < 0) x = - x;
+	return x;
+}
+
+struct DisjointSetUnion {
+	struct Node {
+		int fa;
+	} node[30];
+
+	void Init() {
+		for (int i = 1; i <= 26; i++) node[i].fa = i;
+		return;
+	}
+
+	int Find(int u) {
+		if (node[u].fa == u) return u;
+		return node[u].fa = Find(node[u].fa);
+	}
+
+	void Union(int u, int v) {
+		u = Find(u);
+		v = Find(v);
+		if (u == v) return;
+		node[u].fa = v;
+		return;
+	}
+} DSU;
+
+int main() {
+	std::cin >> n;
+    DSU.Init();
+	for (int i = 1; i <= n; i++) {
+		std::cin >> m;
+		int u = m.front() - 'a' + 1;
+		int v = m.back() - 'a' + 1;
+		graph::addEdge(u, v, m);
+        DSU.Union(u, v);
+	}
+	for (int i = 1; i <= 26; i++) {
+		int d = graph::vertex[i].i - graph::vertex[i].o;
+		if (abs(d) > 1) {
+			std::cout << "***" << '\n';
+			return 0;
+		}
+		if (d == - 1) {
+			if (s) {
+				std::cout << "***" << '\n';
+				return 0;
+			}
+			s = i;
+		}
+		if (d == 1) {
+			if (t) {
+				std::cout << "***" << '\n';
+				return 0;
+			}
+			t = i;
+		}
+	}
+	if (s == 0) for (int i = 1; i <= 26; i++) if (graph::vertex[i].o && t != i) {s = i; break;}
+	for (int i = 1; i <= 26; i++) std::sort(graph::vertex[i].edge.begin(), graph::vertex[i].edge.end(), graph::cmp);
+	for (int i = 1; i <= 26; i++) {
+		if (graph::vertex[i].i == 0 && graph::vertex[i].o == 0) continue;
+		for (int j = i + 1; j <= 26; j++) {
+			if (graph::vertex[j].i == 0 && graph::vertex[j].o == 0) continue;
+			if (DSU.Find(i) != DSU.Find(j)) {
+				std::cout << "***" << '\n';
+				return 0;
+			}
+		}
+	}
+	graph::DFS(s, - 1);
+	graph::print();
+	return 0;
+}
